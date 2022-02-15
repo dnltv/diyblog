577908f345918f1bede1837c691a102fe5ee7699
Danil Treskov <treskovdanilv@gmail.com>
2022-02-16 00:03:37 +0500

    views + templates

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, UpdateView, DeleteView
-from .models import Blogger, Blog
+from .models import Blogger, Blog, Comment
 
 
 # Create your views here.
@@ -9,13 +9,16 @@ def index(request):
     Index page view function.
     """
     text = "That's my new project index page!"
-    #num_of_bloggers = Blogger.objects
-    #num_of_blogs =
-    #num_of_comments =
+    num_of_bloggers = Blogger.objects.count()
+    num_of_blogs = Blog.objects.count()
+    num_of_comments = Comment.objects.count()
     return render(request,
                   'index.html',
                   context={
                       'text': text,
+                      'num_of_bloggers': num_of_bloggers,
+                      'num_of_blogs': num_of_blogs,
+                      'num_of_comments': num_of_comments,
     })
 
 
